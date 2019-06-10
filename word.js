c216dcb135837f70a7f678ef6d988685890f6399
MattJ <47984960+mjalbert13@users.noreply.github.com>
2019-06-10 15:52:08 -0400

    added word constructor

--- word.js
+++ word.js
@@ -1,1 +1,41 @@
-var letters = require("./letters");
+var Letters = require("./letters");
+
+var Word = function(word){
+
+    this.letter = [];
+
+    for(var i =0; i < word.length; i++){
+        if(word.charAt(i) ===" "){
+            this.letter.push(" ");
+        }
+        else{
+            this.letter.push(new Letters(word.sharAt(i)));
+        };
+    };
+
+    this.displayWord = function(){
+
+        var hiddenWord = "";
+
+        for(var i =0; i < this.letter.length; i++){
+            if(this.letter[i].letter === null || this.letter[i].letter===undefined){
+                hiddenWord+=" ";
+            }
+            else{
+                hiddenWord += this.letter[i].displayLetter;
+            };
+        };
+        return hiddenWord;
+    };
+
+    this.guessWord = function(tryLetter){
+
+        for(var i=0; i< this.letter.length; i++){
+            if(this.letter[i].letter !== undefined){
+                this.letter[i].letterGuess(tryLetter);
+            };
+        };
+    };
+};
+
+ module.exports =Word;
